b8dc91aae879951d7c5233c57980044141fcd2ab
Swapnil Kotwal <66004966+Swapnil-25@users.noreply.github.com>
2023-05-30 14:24:26 +0530

    Update Problem2.rb

--- Problem2.rb
+++ Problem2.rb
@@ -1,18 +1,32 @@
 =begin
     Given the following data structures, write a program that copies the information 
     from the array into the empty hash that applies to the correct person.
-=end
-contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+=end 
+class ArrayToHash                                                      # class
+    
+    def convertArrayToHash(contact_data,contacts)                      # Method to convert array to hash
+        i=0
+        contacts.each do |key,value|                                   # adding values in hash
+            contacts[key][:email] = contact_data[i][0]
+            contacts[key][:address] = contact_data[i][1]
+            contacts[key][:phone] = contact_data[i][2]
+            i+=1
+        end 
+    end
+    
+    def printHash(contacts)                                           # Method to print hash
+        puts contacts
+        # contacts.each do |key,value|
+        #     puts "Key: #{key}  value: #{value}"
+        # end    
+    end   
+end    
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],             # Given array
             ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-i=0
-contacts.each do |key,value|                                   # Traversing and adding values in hash
-    # puts "#{key},#{value}"
-    contacts[key][:email] = contact_data[i][0]
-    contacts[key][:address] = contact_data[i][1]
-    contacts[key][:phone] = contact_data[i][2]
-    i+=1
-end 
-puts contacts                             # Printing output
+arrayToHashObj=ArrayToHash.new                                      # Creating Object
+arrayToHashObj.convertArrayToHash(contact_data,contacts)            # Method calling
+arrayToHashObj.printHash(contacts)                                  # Printing Hash
