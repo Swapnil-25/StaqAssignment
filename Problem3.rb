c4d63d9e6dc376f5db55d727fd5a516f97a07ca3
Swapnil Kotwal <66004966+Swapnil-25@users.noreply.github.com>
2023-05-30 14:33:13 +0530

    Update Problem3.rb

--- Problem3.rb
+++ Problem3.rb
@@ -6,17 +6,20 @@
 
 =end
 
-def remove_first_last(str,strLength)       #Function to remove first and last two characters from string
-    return str.slice(2..strLength)
-end
+class RemoveFirstandLastChar
+     def removeFirstLast(str,strLength)                                       #Function to remove first and last two characters from string
+        return str.slice(2..strLength)
+    end
+end    
 
+RemoveFirstandLastCharObj = RemoveFirstandLastChar.new                          # Creating object of class
 puts "Enter string : "           
-str = gets.chomp                           # Getting input from user
+str = gets.chomp                                                                # Getting input from user
 strLength = str.length
 if(strLength<=4)
-    puts "String length is less than 4."
+    puts "String length is less than 4. Enter another string"
 else    
-    puts "After removing the first and last two characters from string."     
-    result = remove_first_last(str,strLength-3)             # Calling function
-    puts result                                             # Printing result
+    puts "After removing the first and last two characters from string:"     
+    result = RemoveFirstandLastCharObj.removeFirstLast(str,strLength-3)       # Calling Method
+    puts result                                                                 # Printing result
 end  
