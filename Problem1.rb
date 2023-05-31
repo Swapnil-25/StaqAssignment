ab80e6b3f249bc0230ae782e045fc928b6e45f41
Swapnil Kotwal <66004966+Swapnil-25@users.noreply.github.com>
2023-05-31 14:24:40 +0530

    Update Problem1.rb

--- Problem1.rb
+++ Problem1.rb
@@ -6,41 +6,44 @@ Suppose you have a hash h = {a:1, b:2, c:3, d:4}
 =end
 
 class HashProblem
+    def initialize(hash)
+        @hash = hash
+    end   
     
-    # Method to print value of b
-    def printValueOfKeyB(hash)
-        puts "Value of key ':b' : #{hash[:b]}";
+    # Method to print value of key
+    def print_value_of_key(key)
+        puts "Value of key '#{key}' is  #{@hash[key]}";
     end
     
     # Method to add value in hash 
-    def AddingValueToHash(hash)
-        hash[:e]=5;      
+    def add_value_to_hash(key,value)
+        @hash[key]=value;      
     end
     
     # Method to delete elements whose value is less than 3.5
-    def DeleteElements(hash)
-        hash.delete_if{|key,value| value<3.5}
+    def delete_pairs_less_than(value)
+        @hash.delete_if{|key,val| val<value}
     end    
 
-    def printHash(hash)
-        hash.each do |key,value|
+    def print_hash
+        @hash.each do |key,value|
             puts "Key: #{key}  value: #{value}"
         end    
     end    
 end    
 
-hash = { a:1, b:2, c:3, d:4 }                           # Given Hash
-hashProblemobj=HashProblem.new                          # Creating object of class
-# 1. Get value of key `:b`
-hashProblemobj.printValueOfKeyB(hash)                   # Calling Method
-puts ""
+hash = { a:1, b:2, c:3, d:4 }
+hashProblem=HashProblem.new(hash)
+
+# 1. Get value of key 
+hashProblem.print_value_of_key(:b)                   
 
 # 2. Add to this hash the key:value pair `{e:5}`
-hashProblemobj.AddingValueToHash(hash)                  # Calling Method
+hashProblem.add_value_to_hash(:e,5)
 puts "Hash after adding key:value pair `{e:5}` :"
-hashProblemobj.printHash(hash)
+hashProblem.print_hash
+
 # 3. Remove all key:value pairs whose value is less than 3.5
-hashProblemobj.DeleteElements(hash)                                 # Calling Method
-puts ""
+hashProblem.delete_pairs_less_than(3.5)
 puts "Hash after deleting elements whose value is less than 3.5"
-hashProblemobj.printHash(hash)
+hashProblem.print_hash
